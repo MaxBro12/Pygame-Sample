8ee02e35a9cb76afa581eb86c7d393f58aaf9662
MaxBro <activer12@mail.ru>
2023-02-23 14:46:07 +0300

    updatemethod

--- pygametest/core/game.py
+++ pygametest/core/game.py
@@ -11,16 +11,27 @@ class Game:
         self.config = config
 
         # ? Настраиваем конфиг
-        self.screen = pygame.display.set_mode(
-            (config['GAME']['width'], config['GAME']['height'])
-        )
-        pygame.display.set_caption('Pygame Sample')
+        self.screen = pygame.display
+        self.screen.set_caption('Pygame Sample')
+        self.update_conf()
         self.clock = pygame.time.Clock()
         self.level = Level()
 
     def update_conf(self):
         if self.config['GAME']['fullscreen']:
-            self.screen.set_m
+            self.screen.set_mode(flags=pygame.FULLSCREEN)
+        else:
+            self.screen.set_mode(
+                (self.config['GAME']['width'], self.config['GAME']['height']),
+            )
+            if self.config['GAME']['resizable']:
+                self.screen.set_mode(
+                    flags=pygame.RESIZABLE,
+                )
+            if self.config['GAME']['vsync']:
+                self.screen.set_mode(
+                    vsync=1,
+                )
 
     def run(self):
         while True:
